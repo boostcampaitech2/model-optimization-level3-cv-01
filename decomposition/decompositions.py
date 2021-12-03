057d26dcf892bdc083f8b62ce13f352a9b59ca0e
SuhYoon Jeon <49219342+euniejeon@users.noreply.github.com>
2021-12-03 16:48:36 +0900

    Create: decompositions.py

---
+++ decomposition/decompositions.py
@@ -0,0 +1,122 @@
+import tensorly as tl
+from tensorly.decomposition import parafac, partial_tucker
+import numpy as np
+import torch
+import torch.nn as nn
+from decomposition.VBMF import VBMF
+
+def cp_decomposition_conv_layer(layer, rank):
+    """ Gets a conv layer and a target rank, 
+        returns a nn.Sequential object with the decomposition """
+
+    # Perform CP decomposition on the layer weight tensorly. 
+    last, first, vertical, horizontal = \
+        parafac(layer.weight.data, rank=rank, init='svd')
+
+    pointwise_s_to_r_layer = torch.nn.Conv2d(in_channels=first.shape[0], \
+            out_channels=first.shape[1], kernel_size=1, stride=1, padding=0, 
+            dilation=layer.dilation, bias=False)
+
+    depthwise_vertical_layer = torch.nn.Conv2d(in_channels=vertical.shape[1], 
+            out_channels=vertical.shape[1], kernel_size=(vertical.shape[0], 1),
+            stride=1, padding=(layer.padding[0], 0), dilation=layer.dilation,
+            groups=vertical.shape[1], bias=False)
+
+    depthwise_horizontal_layer = \
+        torch.nn.Conv2d(in_channels=horizontal.shape[1], \
+            out_channels=horizontal.shape[1], 
+            kernel_size=(1, horizontal.shape[0]), stride=layer.stride,
+            padding=(0, layer.padding[0]), 
+            dilation=layer.dilation, groups=horizontal.shape[1], bias=False)
+
+    pointwise_r_to_t_layer = torch.nn.Conv2d(in_channels=last.shape[1], \
+            out_channels=last.shape[0], kernel_size=1, stride=1,
+            padding=0, dilation=layer.dilation, bias=True)
+
+    pointwise_r_to_t_layer.bias.data = layer.bias.data
+
+    depthwise_horizontal_layer.weight.data = \
+        torch.transpose(horizontal, 1, 0).unsqueeze(1).unsqueeze(1)
+    depthwise_vertical_layer.weight.data = \
+        torch.transpose(vertical, 1, 0).unsqueeze(1).unsqueeze(-1)
+    pointwise_s_to_r_layer.weight.data = \
+        torch.transpose(first, 1, 0).unsqueeze(-1).unsqueeze(-1)
+    pointwise_r_to_t_layer.weight.data = last.unsqueeze(-1).unsqueeze(-1)
+
+    new_layers = [pointwise_s_to_r_layer, depthwise_vertical_layer, \
+                    depthwise_horizontal_layer, pointwise_r_to_t_layer]
+    
+    return nn.Sequential(*new_layers)
+
+def estimate_ranks(layer):
+    """ Unfold the 2 modes of the Tensor the decomposition will 
+    be performed on, and estimates the ranks of the matrices using VBMF 
+    """
+
+    weights = layer.weight.data
+    unfold_0 = tl.base.unfold(weights, 0) 
+    unfold_1 = tl.base.unfold(weights, 1)
+    _, diag_0, _, _ = VBMF.EVBMF(unfold_0)
+    _, diag_1, _, _ = VBMF.EVBMF(unfold_1)
+    ranks = [diag_0.shape[0], diag_1.shape[1]]
+    return ranks
+
+def tucker_decomposition_conv_layer(layer, rank = None):
+    """ Gets a conv layer, 
+        returns a nn.Sequential object with the Tucker decomposition.
+        The ranks are estimated with a Python implementation of VBMF
+        https://github.com/CasvandenBogaard/VBMF
+    """
+
+    if layer.groups != 1:
+        return layer
+
+    
+    if not rank:
+        ranks = estimate_ranks(layer)
+        if ranks[0] == 0 and ranks[1] == 0:
+            print('error')
+        elif ranks[0] == 0 :
+            ranks[0] = ranks[1]
+        elif ranks[1] == 0:
+            ranks[1] = ranks[0]
+        print(layer, "VBMF Estimated ranks", ranks)
+    elif len(rank) == 2 :
+        ranks = rank
+    elif len(rank) == 1:
+        r1 = int(layer.weight.data.shape[0] * rank[0]) if int(layer.weight.data.shape[0] * rank[0]) > 1 else layer.weight.data.shape[0]
+        r2 = int(layer.weight.data.shape[1] * rank[0]) if int(layer.weight.data.shape[1] * rank[0]) > 1 else layer.weight.data.shape[1]
+        ranks = [r1,r2]
+
+    core, [last, first] = \
+        partial_tucker(layer.weight.data, \
+            modes=[0, 1], rank=ranks, init='svd')
+
+    # print(layer.weight.data.shape,first.shape, core.shape, last.shape)
+    # A pointwise convolution that reduces the channels from S to R3
+    first_layer = torch.nn.Conv2d(in_channels=first.shape[0], \
+            out_channels=first.shape[1], kernel_size=1,
+            stride=1, padding=0, dilation=layer.dilation, bias=False)
+
+    # A regular 2D convolution layer with R3 input channels 
+    # and R3 output channels
+    core_layer = torch.nn.Conv2d(in_channels=core.shape[1], \
+            out_channels=core.shape[0], kernel_size=layer.kernel_size,
+            stride=layer.stride, padding=layer.padding, dilation=layer.dilation,
+            bias=False)
+
+    # A pointwise convolution that increases the channels from R4 to T
+    last_layer = torch.nn.Conv2d(in_channels=last.shape[1], \
+        out_channels=last.shape[0], kernel_size=1, stride=1,
+        padding=0, dilation=layer.dilation, bias=True)
+
+    if layer.bias != None:
+        last_layer.bias.data = layer.bias.data
+
+    first_layer.weight.data = \
+        torch.transpose(first, 1, 0).unsqueeze(-1).unsqueeze(-1)
+    last_layer.weight.data = last.unsqueeze(-1).unsqueeze(-1)
+    core_layer.weight.data = core
+
+    new_layers = [first_layer, core_layer, last_layer]
+    return nn.Sequential(*new_layers)
